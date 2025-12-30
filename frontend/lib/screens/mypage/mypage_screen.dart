74d606f75338d304eea5e2011c4e59fc22aaf829
Yujin <0505ujin@naver.com>
2025-12-30 23:52:23 +0900

    design: ui 구조 잡기

--- frontend/lib/screens/mypage/mypage_screen.dart
+++ frontend/lib/screens/mypage/mypage_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class MypageScreen extends StatelessWidget{
+class MypageScreen extends StatelessWidget {
   const MypageScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const Center(
-      child: Text('MYpage'),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 상단(프로필, 북마크)
+            Container(
+              child: Column(
+                children: [
+                  Row(), // profile
+                  Row(),
+                ],
+              ),
+            ),
+            Container(
+              height: 12,
+              color: Color(0xFFF9FAFB),
+            ),
+            // 하단(약관, 로그아웃)
+            Column(
+              children: [
+
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
