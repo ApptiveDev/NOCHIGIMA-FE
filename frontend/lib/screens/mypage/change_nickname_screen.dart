7541e064f517c3e7bc7a634e5e8a4ff21b3747e9
Yujin <0505ujin@naver.com>
2026-01-01 23:53:11 +0900

    design: listview menu

---
+++ frontend/lib/screens/mypage/change_nickname_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChangeNicknameScreen extends StatefulWidget {
+  const ChangeNicknameScreen({super.key});
+
+  @override
+  State<ChangeNicknameScreen> createState() => _ChangeNicknameScreenState();
+}
+
+class _ChangeNicknameScreenState extends State<ChangeNicknameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
--- frontend/lib/screens/mypage/edit_profile_screen.dart
+++ frontend/lib/screens/mypage/edit_profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:frontend/screens/mypage/change_nickname_screen.dart';
+
 
 class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({super.key});
@@ -37,17 +39,83 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
           child: SizedBox(
             width: double.infinity,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(height: 30),
-                SvgPicture.asset("assets/images/myProfile.svg", width: 140),
+                Center(
+                  child: SvgPicture.asset(
+                    "assets/images/myProfile.svg",
+                    width: 140,
+                  ),
+                ),
                 SizedBox(height: 30),
                 // info
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "기본정보",
+                        style: TextStyle(
+                          fontFamily: "Pretendard",
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF949BA8),
+                        ),
+                      ),
+                      _buildEditMenu(
+                        label: "닉네임",
+                        trailing: Text(
+                          "모아부기",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Pretendard",
+                            fontSize: 16,
+                            color: Color(0xFF323439),
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChangeNicknameScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
                 Container(height: 12, color: Color(0xFFF9FAFB)),
                 // alarm
+                Container(
+                  padding: EdgeInsets.only(top: 14),
+                  child: Column(
+                    children: [
+                      Text(
+                        "알림",
+                        style: TextStyle(
+                          fontFamily: "Pretendard",
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF949BA8),
+                        ),
+                      ),
+
+                    ],
+                  ),
+
+                ),
                 Container(height: 12, color: Color(0xFFF9FAFB)),
                 // social account
-                
+                _buildEditMenu(
+                  label: "연동된 소셜 계정",
+                  trailing: SvgPicture.asset(
+                    "assets/images/kakao_icon_mini.svg",
+                    width: 36,
+                  ),
+                  onTap: () {},
+                ),
                 // 탈퇴하기
               ],
             ),
@@ -56,4 +124,32 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       ),
     );
   }
+}
+
+Widget _buildEditMenu({
+  required String label,
+  required Widget trailing,
+  required VoidCallback onTap,
+}) {
+  return ListTile(
+    contentPadding: EdgeInsets.zero,
+    onTap: onTap,
+    title: Text(
+      label,
+      style: TextStyle(
+        fontSize: 16,
+        fontFamily: "Pretendard",
+        fontWeight: FontWeight.w500,
+        color: Color(0xFF686D78),
+      ),
+    ),
+    trailing: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        trailing,
+        SizedBox(width: 4),
+        Icon(Icons.chevron_right_rounded, color: Color(0xFF858C9A), size: 20),
+      ],
+    ),
+  );
 }
